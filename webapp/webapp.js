29ec1874c763e95f1611110d4cf9ce9db19582fe
ngohuynhngockhanh <ngohuynhngockhanh@gmail.com>
2017-03-07 19:32:14 +0700

    update webapp

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -22,6 +22,9 @@ angular.module('myApp', [
 	//dùng để đặt các giá trị mặc định
     $scope.CamBienMua = "Không biết nữa ahihi, chưa thấy có thằng nào cập nhập hết";
     $scope.leds_status = [1, 1]
+	$scope.lcd = ["", ""]
+	$scope.servoPosition = 0
+	$scope.buttons = [] //chả có gì cả, arduino gửi nhiêu thì nhận nhiêu!
 	
 	////Khu 2 -- Cài đặt các sự kiện khi tương tác với người dùng
 	//các sự kiện ng-click, nhấn nút
@@ -29,6 +32,8 @@ angular.module('myApp', [
 		mySocket.emit("RAIN")
 	}
 	
+	
+	//Cách gửi tham số 1: dùng biến toàn cục! $scope.<tên biến> là biến toàn cục
 	$scope.changeLED = function() {
 		console.log("send LED ", $scope.leds_status)
 		
@@ -36,6 +41,29 @@ angular.module('myApp', [
 			"led": $scope.leds_status
 		}
 		mySocket.emit("LED", json)
+	}
+	
+	//cập nhập lcd như một ông trùm 
+	$scope.updateLCD = function() {
+		
+		
+		var json = {
+			"line": $scope.lcd
+		}
+		console.log("LCD_PRINT ", $scope.lcd)
+		mySocket.emit("LCD_PRINT", json)
+	}
+	
+	//Cách gửi tham số 2: dùng biến cục bộ: servoPosition. Biến này đươc truyền từ file home.html, dữ liệu đươc truyền vào đó chính là biến toàn cục $scope.servoPosition. Cách 2 này sẽ giúp bạn tái sử dụng hàm này vào mục đích khác, thay vì chỉ sử dụng cho việc bắt sự kiện như cách 1, xem ở Khu 4 để biết thêm ứng dụng!
+	$scope.updateServo = function(servoPosition) {
+		
+		var json = {
+			"degree": servoPosition,
+			"message": "Goc ne: " + servoPosition
+		}
+		
+		console.log("SEND SERVO", json) //debug chơi à
+		mySocket.emit("SERVO", json)
 	}
 	
 	////Khu 3 -- Nhận dữ liệu từ Arduno gửi lên (thông qua ESP8266 rồi socket server truyền tải!)
@@ -49,12 +77,20 @@ angular.module('myApp', [
 		console.log("recv LED", json)
 		$scope.leds_status = json.data
 	})
+	//khi nhận được lệnh Button
+	mySocket.on('BUTTON', function(json) {
+		//Nhận được thì in ra thôi hihi.
+		console.log("recv BUTTON", json)
+		$scope.buttons = json.data
+	})
 	
 	
 	//// Khu 4 -- Những dòng code sẽ được thực thi khi kết nối với Arduino (thông qua socket server)
 	mySocket.on('connect', function() {
 		console.log("connected")
 		mySocket.emit("RAIN") //Cập nhập trạng thái mưa
+		
+		$scope.updateServo(0) //Servo quay về góc 0 độ!. Dùng cách 2 
 	})
 		
 });
