32545e03b517221026b318a15615efd366658d11
hòa nguyễn xuân <xuan.n.hoa@gmail.com>
2017-05-25 14:13:37 +0700

    get to better

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -41,11 +41,11 @@ angular.module('myApp', [
 	}
 	$scope.thietbi1on  = function() {
 		mySocket.emit("THIETBI1ON")
-		$scope.Thietbi1 = "ON"
+		$scope.Thietbi1 = json.data
 	}
 	$scope.thietbi1off  = function() {
 		mySocket.emit("THIETBI1OFF")
-		$scope.Thietbi1 = "OFF"
+		$scope.Thietbi1 = json.data
 	}
 
 	$scope.thietbi2on  = function() {
