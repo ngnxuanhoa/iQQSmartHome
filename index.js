a279239299442d0ed1a2a258038a5fbec4eeffa9
unknown <xuan.n.hoa@gmail.com>
2019-10-27 07:04:30 +0700

    make it better

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ var io = socketio(server);								//#Phải khởi tạo io sau khi tạo app
 
 var webapp_nsp = io.of('/webapp')				//namespace của webapp
 var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
-var rqmobile_nsp = io.of('/rqmobile')
+var rqmobile_nsp = io.of('/rqmobile')				//namespace của esp8266
 
 var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
 esp8266_nsp.use(middleware);									//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
 webapp_nsp.use(middleware);									//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
 rqmobile_nsp.use(middleware);
-
-server.listen(PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
+ 
+server.listen(process.env.PORT || PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
 console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
 
 //Cài đặt webapp các fie dữ liệu tĩnh
@@ -50,13 +50,13 @@ esp8266_nsp.on('connection', function(socket) {
 	})
 	
 	//nhận được bất cứ lệnh nào
-	socket.on("*", function(packet) {
+	socket.on('*', function(packet) {
 		console.log("esp8266 rev and send to webapp packet: ", packet.data) //in ra để debug
 		var eventName = packet.data[0]
 		var eventJson = packet.data[1] || {} //nếu gửi thêm json thì lấy json từ lệnh gửi, không thì gửi chuỗi json rỗng, {}
 		webapp_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến webapp
 		rqmobile_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến webapp
-	})
+	});
 })
 
 //Bắt các sự kiện khi webapp kết nối
@@ -78,20 +78,23 @@ webapp_nsp.on('connection', function(socket) {
 		rqmobile_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến webapp
 	});
 })
+//Bắt các sự kiện khi webapp kết nối
+
 rqmobile_nsp.on('connection', function(socket) {
 	
-	console.log('rqmobile connected')
+	console.log('webapp connected')
 	
 	//Khi webapp socket bị mất kết nối
 	socket.on('disconnect', function() {
-		console.log("Disconnect socket rqmobile")
+		console.log("Disconnect socket webapp")
 	})
 	
 	socket.on('*', function(packet) {
-		console.log("Mobile send to esp8266 packet: ", packet.data) //in ra để debug
+		console.log("webapp rev and send to esp8266 packet: ", packet.data) //in ra để debug
 		var eventName = packet.data[0]
 		var eventJson = packet.data[1] || {} //nếu gửi thêm json thì lấy json từ lệnh gửi, không thì gửi chuỗi json rỗng, {}
 		esp8266_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến esp8266
 		webapp_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến webapp
 	});
 })
+console.log("ok")
